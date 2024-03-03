e2ebd94e2fd9ec24dc2854a586bfb8c58b2a1d2b
liamtphillips <liamtphillips@hotmail.com>
2024-03-03 19:04:07 +0000

    navbar and scrollToTop completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import { NavLink, Link } from "react-router-dom"; 
+import { FaHome } from "react-icons/fa";
+import { RiTeamFill } from "react-icons/ri";
+import { FaRegQuestionCircle } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa6";
+
+
+const activeNavLink = ({ isActive }) => (isActive ? "active_link text-indigo-600" : "");
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="sticky top-0 right-0 w-full z-50 bg-black/10 backdrop-blur-sm">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-4 px-3">
+      <Link to="/">
+        <span className="ml-1 text-white text-4xl hidden md:block">
+          <span className="text-indigo-600 font-semibold">LP</span>SECURITY
+        </span>
+        </Link>
+        <div className="flex items-center mx-auto md:mx-0">
+          {/* Responsive Navbar for small screens */}
+          <div className="md:hidden flex gap-x-6 sticky top-0 text-white z-50">
+            <NavLink to="/" className={activeNavLink}>
+              <FaHome size={24} />
+            </NavLink>
+            <NavLink to="/team" className={activeNavLink}>
+              <RiTeamFill size={24} />
+            </NavLink>
+            <NavLink to="/about" className={activeNavLink}>
+              <FaRegQuestionCircle size={24} />
+            </NavLink>
+            <NavLink to="/contact" className={activeNavLink}>
+              <FaPhone size={24} />
+            </NavLink>
+          </div>
+          {/* Navbar for medium and above screens */}
+          <ul className="hidden md:flex md:text-md text-white flex-col md:flex-row md:space-x-8 items-center">
+            <li>
+              <Link to="/" className="block py-2 px-3 hover:text-indigo-600 duration-400 transition-all" aria-current="page">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/team" className="block py-2 px-3 hover:text-indigo-600 duration-400 transition-all">
+                Team
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="block py-2 px-3 hover:text-indigo-600 duration-400 transition-all">
+                About
+              </Link>
+            </li>
+            <div className="">
+              <Link
+                to="/contact"
+                className="text-white border-2 text-xl  border-indigo-700 px-3 py-1 rounded-md hover:bg-indigo-700 transition-all duration-500"
+              >
+                Contact
+              </Link>
+            </div>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-const ScrollToTop = () => {
-  return (
-    <div>ScrollToTop</div>
-  )
+export default function ScrollToTop() {
+    const { pathname } = useLocation();
+    useEffect(() => {
+        const scrollToTop = () => {
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+          });
+        };
+    
+        scrollToTop();
+      }, [pathname]);
+      
+    return null;
 }
-
-export default ScrollToTop
